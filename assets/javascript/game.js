4447ca3ad3478e244064aa7e67e1125f96165bb4
mateyo3 <mateoreyes720@gmail.com>
2017-08-28 22:51:58 -0700

    psuedocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+// Press any key to allow inputs
+
+// Generate word
+	// Computer chooses word from array
+
+
+// userGuess
+	// user presses a letter key
+	// Guess count goes down by one unit, no matter what letter is chosen
+	// Guessed letter get stored in array
+	// letters can only be pressed once
+		// Letters get stored in lettersGuessed array
+	// If userGuess matches letter in word
+		// then letter replaces blank placeholder
+	// If guessCount goes below 1
+		// Then game restarts
+
+
+// Display
+	// How to begin game
+		// Show instructions: "Press any key to begin"
+
+
+	// Blank word placeholders
+		// lines change to letter if correct letter is chosen
+
+	// Number of wins
+		// Number of wins increases by one if correct word is guesses
+		// Number of wins stays the same if incorrect
+
+	// Number of guesses remaining
+
+
+	// Letters already guessed
+
+
+	// Guess current word = picture change + music
+
+	// Press any key to reset word
